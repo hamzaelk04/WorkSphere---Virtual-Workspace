a3ae2a7325791c4db65711b483e60cbbdd37a0d8
hamzaelk04 <hamzakabraoui07@gmail.com>
2025-11-20 00:00:27 +0100

    add dynamic input in modal form

--- script.js
+++ script.js
@@ -1,32 +1,53 @@
 function addNewWorker() {
-    formdata()
+    const form = document.forms.namedItem('addAworker');
+    formdata(form)
 
+    let workerListe = getDataFromLocalStorage('worker')
+    let card = document.getElementById('cardsParent')
 
+    card.innerHTML = ""
+    // workerListe.forEach(element => {
+    //     card.innerHTML += `
+    //             <div class="card">
+    //                 <div class="imgUser">
+    //                     <img src="userIcon/${element.photo}.png" class="imgUser" alt="">
+    //                 </div>
+    //                 <div class="infoUserInCard">
+    //                     <p class="nameUser" style="font-weight: bold;">${element.name}</p>
+    //                     <p class="roleUserInCard">${element.role}</p>
+    //                 </div>
+    //                 <div class="editButton">
+    //                     <button class="edit btn btn-warning">
+    //                         Edit
+    //                     </button>
+    //                 </div>
+    //             </div>`
+    // });
 }
 
-addNewWorker()
-
-function formdata() {
-    const form = document.forms.namedItem('addAworker');
-
-    let worker = {}
-
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
+function formdata(form) {
+    form.addEventListener('submit', () => {
+        let worker = {}
 
         worker.name = form.elements["firstName"].value
         worker.photo = form.elements["photo"].value
         worker.phoneNumber = form.elements["phoneNumber"].value
         worker.email = form.elements["email"].value
         worker.role = form.elements["role"].value
-        worker.experience = form.elements["experience"].value
+        // worker.experience = form.elements["experience"].value
 
         saveInLocalStorage("worker", worker)
+
+        
     })
 }
 
 function saveInLocalStorage(key, value) {
-    let data = getDataFromLocalStorage("worker") || []
+    let data = getDataFromLocalStorage("worker")
+
+    if (data == null || data == undefined) {
+        data = []
+    }
 
     data.push(value)
 
@@ -38,3 +59,50 @@ function getDataFromLocalStorage(key) {
 
     return JSON.parse(data)
 }
+
+
+addNewWorker()
+
+
+function dynamicInput() {
+    let btn = document.getElementById('addExperiences')
+
+    btn.addEventListener('click', addExperience)
+}
+
+function addExperience() {
+    let divParent = document.getElementById('newExperience')
+
+    divParent.innerHTML += `
+                        <div class="eachExperience">
+                           <input type="text" class="form-control experiencePost" name="experience"
+                               placeholder="Post">
+                           <input type="text" class="form-control experienceCompany" name="experience"
+                               placeholder="Company">
+                           <button type="button" class="btn btn-danger removeExperience"><i class="fa-solid fa-minus"></i></button>
+                       </div>`
+}
+
+
+
+function modal() {
+    const modal = document.getElementById('exampleModal')
+    modal.addEventListener('shown.bs.modal', function() {
+        dynamicInput()
+    })
+
+    removeExperience()
+}
+
+function removeExperience() {
+    const remove = document.getElementById('newExperience')
+    remove.addEventListener('click', function (e) {
+    if (e.target.closest('.removeExperience')) {
+        e.target.closest('.eachExperience').remove();
+    }
+});
+
+}
+
+
+modal()
